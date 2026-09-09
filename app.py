3154daef760dfe785f3188c82100f03b944c8814
Isaac Costa <isaaccosta.e@gmail.com>
2026-09-09 18:15:33 -0300

    adicionado titulo e botoes

--- app.py
+++ app.py
@@ -2,4 +2,31 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
-df = pd.read_csv("vehicles.csv")
+car_data = pd.read_csv("vehicles.csv")
+
+#titulo adicionado
+[st.header("Vehicle Data Analysis")]
+
+#botao adicionado
+hist_button = st.button("show histogram")
+
+if hist_button:
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.histogram(car_data, x= 'odometer')
+    st.plotly_chart(fig, use_container_width=True) 
+
+disp_button = st.button("show dispersion")
+
+if disp_button:
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.scatter(car_data, x= 'odometer')
+    st.plotly_chart(fig, use_container_width=True)
+    
+build_histogram = st.checkbox('Criar um histograma')
+
+if build_histogram: # se a caixa de seleção for selecionada
+  st.write('Criando um histograma para a coluna odometer')
+  fig = px.histogram(car_data, x= 'odometer')
+  st.plotly_chart(fig, use_container_width=True)
+  
+  
